509b4dfb3c24d6deb6d7d15916a24cd68e9637c5
maustin1 <mtv1243@gmail.com>
2021-03-10 18:09:48 -0500

    begin calculator project to learn OOP in C#

---
+++ PairProgrammingAdamBrennanMtv/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairProgrammingAdamBrennanMtv
+{
+    class Calculator
+    {
+        public int Input1 { get; set; }
+        public int Input2 { get; set; }
+        public char Operator { get; set; }
+    }
+}
