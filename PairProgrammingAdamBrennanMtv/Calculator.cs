9c0a6702dfb549c9b171cec2ee2dd1130ac7c1d2
maustin1 <mtv1243@gmail.com>
2021-03-14 10:30:19 -0400

    create OOP calculator

--- PairProgrammingAdamBrennanMtv/Calculator.cs
+++ PairProgrammingAdamBrennanMtv/Calculator.cs
@@ -11,5 +11,47 @@ namespace PairProgrammingAdamBrennanMtv
         public int Input1 { get; set; }
         public int Input2 { get; set; }
         public char Operator { get; set; }
+
+        // Methods
+        public void ParseInput(string input)
+        {
+            string[] splitInput = input.Split(' ');
+            PopulateCalcValues(splitInput);
+        }
+
+        private void PopulateCalcValues(string[] splitInput)
+        {
+            Input1 = int.Parse(splitInput[0]);
+            Input2 = int.Parse(splitInput[2]);
+            Operator = char.Parse(splitInput[1]);
+            
+        }
+
+        public int Calculate()
+        {
+            switch(Operator)
+            {
+                case ('+'):
+                    return (Input1 + Input2);
+
+                case ('-'):
+                    return (Input1 - Input2);
+
+                case ('*'):
+                    return (Input1 * Input2);
+
+                case ('/'):
+                    return (Input1 / Input2);
+
+                case ('%'):
+                    return (Input1 % Input2);
+
+                // exceptions are errors
+                // will be handled in the Main method
+                // are classes that travel through different modules
+                default:
+                    throw new ArgumentOutOfRangeException("Operator", Operator, "Not a valid operation");
+            }
+        }
     }
 }
--- PairProgrammingAdamBrennanMtv/Program.cs
+++ PairProgrammingAdamBrennanMtv/Program.cs
@@ -11,6 +11,28 @@ namespace PairProgrammingAdamBrennanMtv
         static void Main(string[] args)
         {
             Calculator calculator = new Calculator();
+            while(true)
+            {
+                Console.WriteLine("Enter your problem below with spaces as shown: 2 + 3");
+                string input = Console.ReadLine();
+
+                if(input.Equals("quit"))
+                {
+                    break;
+                }
+
+                int result = 0;
+                try
+                {
+                    calculator.ParseInput(input);
+                    result = calculator.Calculate();
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                Console.WriteLine(result);
+            }
 
             Console.ReadLine();
         }
